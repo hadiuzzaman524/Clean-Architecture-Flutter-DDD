8b3e622f00b82a41797b935c8a03174bffc0af2d
hadi.heavytask <hadi@heavytask.com>
2022-10-12 13:26:33 +0600

    feat: add test case

---
+++ test/features/number_trivia/domain/repositories/get_concreate_number_trivia_repositories_test.dart
@@ -0,0 +1,42 @@
+import 'package:clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_architecture/features/number_trivia/domain/repositories/number_trivia_repositories.dart';
+import 'package:clean_architecture/features/number_trivia/domain/usecases/get_concreate_number_trivia.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+//https://exchangetuts.com/dart-type-null-is-not-a-subtype-of-type-futurestring-in-mockito-1639930888295141
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+@GenerateMocks([NumberTriviaRepository])
+void main() {
+  late GetConcreteNumberTrivia usecase;
+  late MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+
+  const tNumber = 1;
+  const tNumberTrivia = NumberTrivia(number: 1, text: 'test');
+
+  test(
+    'should get trivia for the number from the repository',
+    () async {
+      when(mockNumberTriviaRepository.getConcreteNumberTrivia(any))
+          .thenAnswer((_) async => const Right(tNumberTrivia));
+      // The "act" phase of the test. Call the not-yet-existent method.
+      final result = await usecase.execute(number: tNumber);
+      // UseCase should simply return whatever was returned from the Repository
+      expect(result, const Right(tNumberTrivia));
+      // Verify that the method has been called on the Repository
+      verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+      // Only the above method should be called and nothing more.
+      verifyNoMoreInteractions(mockNumberTriviaRepository);
+    },
+  );
+}
