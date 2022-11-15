89af375f36ec1ad0fc4a1c3a07133dcbe1882923
hadi.heavytask <hadi@heavytask.com>
2022-11-15 16:07:10 +0600

    fix: firebase initialization error

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:clean_architecture/application/auth/sign_in/sign_in_form_bloc.dart';
 import 'package:clean_architecture/presentation/auth/sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'injection_configuration.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   configureDependencies();
   runApp(const MyApp());
 }
--- lib/presentation/auth/sign_in.dart
+++ lib/presentation/auth/sign_in.dart
@@ -1,4 +1,8 @@
+import 'package:clean_architecture/application/auth/sign_in/sign_in_form_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../application/auth/sign_in/sign_in_form_state.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({Key? key}) : super(key: key);
@@ -8,58 +12,65 @@ class SignInScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Form(
-          child: ListView(
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              const Text(
-                "📝",
-                style: TextStyle(fontSize: 70),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(
-                height: 100,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.email),
-                  label: Text("Email"),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(16)),
+        child: BlocConsumer<SignInFormBloc, SignInFormState>(
+          listener: (context, state) {
+            // TODO: implement listener
+          },
+          builder: (context, state) {
+            return Form(
+              child: ListView(
+                children: [
+                  const SizedBox(
+                    height: 50,
                   ),
-                ),
+                  const Text(
+                    "📝",
+                    style: TextStyle(fontSize: 70),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.email),
+                      label: Text("Email"),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(16)),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        prefixIcon: Icon(Icons.password),
+                        label: Text("Password"),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(16)),
+                        )),
+                    obscureText: true,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Login"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Register"),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text("Log In With Google"),
+                  ),
+                ],
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                    prefixIcon: Icon(Icons.password),
-                    label: Text("Password"),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(16)),
-                    )),
-                obscureText: true,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("Login"),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("Register"),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: const Text("Log In With Google"),
-              ),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
